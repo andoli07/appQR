aa8eec6401c155cab2077c28e5c0ca59ee1c4d35
andoli07 <and.olivos@duocuc.cl>
2024-09-06 19:42:50 -0400

    grilla perfil

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -2,13 +2,19 @@ import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { AnimationController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
-
+  cards = [
+    { title: 'Título 1', content: 'Contenido de la tarjeta 1' },
+    { title: 'Título 2', content: 'Contenido de la tarjeta 2' },
+    { title: 'Título 3', content: 'Contenido de la tarjeta 3' },
+    { title: 'Título 4', content: 'Contenido de la tarjeta 4' }
+  ];
   username = '';
   constructor(private router: Router, private animationController: AnimationController) {
     const navegacion = this.router.getCurrentNavigation();
