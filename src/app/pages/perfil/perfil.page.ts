d936dc38b5fca45167f1fd39f5b489046fa66c79
andoli07 <and.olivos@duocuc.cl>
2024-09-06 23:38:19 -0400

    cambios varios

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { AnimationController } from '@ionic/angular';
+import type { IonModal } from '@ionic/angular';
 
 
 @Component({
@@ -10,12 +11,13 @@ import { AnimationController } from '@ionic/angular';
 })
 export class PerfilPage implements OnInit {
   cards = [
-    { title: 'Título 1', content: 'Contenido de la tarjeta 1' },
-    { title: 'Título 2', content: 'Contenido de la tarjeta 2' },
-    { title: 'Título 3', content: 'Contenido de la tarjeta 3' },
-    { title: 'Título 4', content: 'Contenido de la tarjeta 4' }
+    { title: 'Programación Móvil', content: 'Sección 003_D', imgSrc: 'assets/img/tec.webp' },
+    { title: 'Programación Base de Datos', content: 'sección_008A', imgSrc: 'assets/img/tecno.png' },
+    { title: 'Arquitectura', content: 'Sección_002C', imgSrc: 'assets/img/tecnologia.jpg' },
+    { title: 'Portafolio', content: 'Sección_005B', imgSrc: 'assets/img/portafolio.jpg'  }
   ];
   username = '';
+  
   constructor(private router: Router, private animationController: AnimationController) {
     const navegacion = this.router.getCurrentNavigation();
     const state = navegacion?.extras.state as {
